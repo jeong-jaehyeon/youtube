652431439fcc9ad0c0a29f37d1ab2b979fe0e93d
정재현 <wolgus104@naver.com>
2022-05-23 18:27:12 +0900

    onClick, onKeyPress, handleSearch 작동 확인.

--- src/components/search_header/search_header.jsx
+++ src/components/search_header/search_header.jsx
@@ -1,13 +1,24 @@
 import styles from './search_header.module.css'
-import React from 'react';
+import React, { useRef } from 'react';
 
 const SearchHeader = (props) => {
+    const inputRef = useRef();
+    // 검색 인풋 필드 안에 값을 inputRef로 useRef를 이용하여 받아놓고.
+    const handleSearch = () => {
+        const value = inputRef.current.value;
+        console.log(value + '핸들 서치 실행됩니다.');
+    }
+
     const onClick = () => {
-        console.log('온클릭 잘되니?');
+        console.log('온클릭 실행됩니다.');
+        handleSearch();
     };
 
-    const onKeyPress = () => {
-        console.log('온키프레스 잘되니?');
+    const onKeyPress = (event) => {
+        console.log('온키프레스 실행됩니다.');
+        if (event.key === 'Enter')  {
+            handleSearch();
+        }
     };
 
     return (
@@ -16,7 +27,8 @@ const SearchHeader = (props) => {
                 <img className={styles.img} src='./images/logo.png' alt='유튜브 아이콘'></img>
                 <h1 className={styles.title}>Youtube</h1>
             </div>
-            <input className={styles.input} type='search' placeholder='Search...' onKeyPress={onKeyPress}></input>
+            <input className={styles.input} type='search' placeholder='Search...' onKeyPress={onKeyPress} ref={inputRef}></input>
+            {/* ref 선언 */}
             <button className={styles.button} type='submit' onClick={onClick}>
                 <img className={styles.buttonImage} src='./images/search.png' alt='확인 버튼 이미지'></img>
             </button>
