40bb52f1e65457b24198419f3c2cdd21efa4f2a5
정재현 <wolgus104@naver.com>
2022-06-08 10:44:26 +0900

    유튜브 api 비동기로 제어

--- src/service/youtube.js
+++ src/service/youtube.js
@@ -18,26 +18,22 @@ class Youtube {
     }
 
     // 인자를 받아서 검색해주는
-    search_youtube(query) {
-        return fetch(
+    async search_youtube(query) {
+        const response = await fetch(
             `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=${this.key}`,
             this.getRequestOptions
-        )
-            .then(response => response.json())
-            .then(result =>
-                result.items.map(item => ({ ...item, id: item.id.videoId }))
-            );
+        );
+        const result_1 = await response.json();
+        return result_1.items.map(item => ({ ...item, id: item.id.videoId }));
     }
 
     // videoType --> movie 인 것만.
-    search_youtbue_movie(query) {
-        return fetch(
-            `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=${query}&type=video&videoType=movie&key=${this.key}`
-            , this.getRequestOptions)
-            .then(response => response.json())
-            .then(result =>
-                result.items.map(item => ({ ...item, id: item.id.videoId }))
-            )
+    async search_youtbue_movie(query) {
+        const response = await fetch(
+            `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=${query}&type=video&videoType=movie&key=${this.key}`,
+            this.getRequestOptions);
+        const result_1 = await response.json();
+        return result_1.items.map(item => ({ ...item, id: item.id.videoId }));
     }
 }
 
